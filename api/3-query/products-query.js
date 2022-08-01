73ad021da95b99812d27a20ee984ccf6497305bb
daniela toro <daniela.toro@mercadolibre.com.co>
2022-08-01 16:09:07 -0500

    Query practice

--- api/3-query/products-query.js
+++ api/3-query/products-query.js
@@ -1,3 +1,5 @@
+const router = require('nordic/ragnar').router();
+
 /**
 Ejercicio 3 - Query
 
@@ -40,3 +42,67 @@ Despues de implementar el código, correr el comando
 `npm run test:unit:watch products-query-route` y comprobar 
 que pasan todos los tests.
 ********************************************************/
+
+
+let products = [
+    {
+        name: "Mouse",
+        price: 4598,
+        stock: 30,
+    },
+    {
+        name: "Notebook",
+        price: 2598,
+        stock: 30,
+    },
+    {
+        name: "Macbook",
+        price: 9898,
+        stock: 0,
+    },
+];
+
+// La información viaja por query
+// https://dev.mercadolibre.com.ar:8443/api/products-query?id=400&name
+// El signo ? indica que lo que viene después son los parámetros
+router.get('/', (req, res) => {
+    const { name, minPrice, maxPrice} = req.query;
+    const filterDB = products.filter(product => product.stock > 0);
+    if (filterDB.length === 0){
+        res.json('No se encontraron productos')
+    } else {
+        if (!name && !minPrice && !maxPrice){
+            res.json(filterDB)
+        } else {
+            const filterName = filterDB.filter(product => product.name === name);
+            if (filterName.length === 0){
+                res.json('No se encontraron productos')
+            } else {
+                console.log('aaaaaaaaaaaa', filterName)
+                res.json(filterName)
+            }
+        }
+    }
+})
+
+router.get('/', (req,res) => {
+    const { minPrice, maxPrice } = req.query;
+    const filterPrice = products.filter(product => {
+        if (product.price >= minPrice && product.price <= maxPrice){
+            product
+        }
+    })
+    if (filterPrice.length === 0) {
+        res.json('No se encontraron productos')
+    } else {
+        res.json(filterPrice);
+    }
+})
+
+// La información viaja por params y es obligatoria ya que no lleva el ?
+/* router.get('/:id/:name', (req, res) => {
+    const { id, name } = req.params;
+    res.json({id, name});
+}) */
+
+module.exports = router;
