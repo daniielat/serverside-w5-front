6ca00c011fa73db7a2b315ceb60383e1483dd444
daniela toro <daniela.toro@mercadolibre.com.co>
2022-08-02 21:53:55 -0500

    First Commit

---
+++ app/pages/home2/view.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+///Creamos la View y le pasamos los props
+function View(props){
+    const { title } = props; // Desestructurar las props
+
+    return (
+        <h2>{title}</h2>
+    )
+}
+
+
+module.exports = View;
